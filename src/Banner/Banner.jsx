158b152f1d42af214e84031b7b4a10fba4e20c35
Mardixx <kimi.lefort.pro@gmail.com>
2023-12-19 10:21:20 +0100

    fix

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = ({ moviesInfo }) => {
         return (
             <div className="banner">
                 <Link to = '/discover' state = {{from: moviesInfo.id}} ><img className="backdrop" src={`https://image.tmdb.org/t/p/w500/${moviesInfo.backdrop_path}`} /></Link>
-                <h3 className="titleBanner"><img className="playButton" src="/public/play-solid.svg" />{moviesInfo.original_title}</h3>
+                <h3 className="titleBanner"><img className="playButton" src="/play-solid.svg" />{moviesInfo.original_title}</h3>
             </div>
         )
 }
